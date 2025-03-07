d687126759b08f165bfa831c4d8d33fea3e73cf0
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-03-07 15:55:45 +0300

    function-continued submission

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// function declaration 
+function saturdayFun(fun = "roller-skate" ){
+    return (`This Saturday, I want to ${fun}!`)
+}
+
+//function expression
+const mondayWork = function (office = "go to the office"){
+    return (`This Monday, I will ${office}.`)
+}
+function wrapAdjective(wrap = "*"){
+
+    return function(adj = "special"){
+        return (`You are ${wrap}${adj}${wrap}!`);
+    }
+    
+
+}
